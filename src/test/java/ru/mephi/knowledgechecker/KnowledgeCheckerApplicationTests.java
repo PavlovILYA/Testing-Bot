827f1c5d55d1db2ff8314ebb43ba635c23d8064a
Илюша <79216023478@yandex.ru>
2022-11-05 22:14:37 +0300

    feat: добавить структуру проекта

---
+++ src/test/java/ru/mephi/knowledgechecker/KnowledgeCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mephi.knowledgechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
