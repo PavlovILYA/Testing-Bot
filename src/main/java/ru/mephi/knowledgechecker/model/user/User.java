b58558c4fb600504b2356189aea9ff228e10e2c5
Илюша <79216023478@yandex.ru>
2022-11-29 23:46:57 +0300

    fix: нормализовать таблицы БД (добавить в каждую таблицу независящий по смыслу id)

--- src/main/java/ru/mephi/knowledgechecker/model/user/User.java
+++ src/main/java/ru/mephi/knowledgechecker/model/user/User.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -18,7 +17,6 @@ import javax.persistence.Table;
 @Builder(toBuilder = true)
 public class User {
     @Id
-    @Column(name = "chat_id")
     private Long id;
     private String firstName;
     private String lastName;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,24 @@
 CREATE TABLE IF NOT EXISTS users (
-    chat_id BIGINT UNIQUE, -- from tg chat
+    id BIGINT UNIQUE, -- from tg chat
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     username VARCHAR(255) UNIQUE,
-    PRIMARY KEY (chat_id)
+    PRIMARY KEY (id)
 );
 
 CREATE INDEX IF NOT EXISTS users_username
-    ON users (username);
-
--- CREATE TABLE IF NOT EXISTS test_types (
---     type VARCHAR(255) UNIQUE,
---     PRIMARY KEY (type)
--- );
+    ON users(username);
 
 CREATE TABLE IF NOT EXISTS tests (
+    id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE,
     unique_title VARCHAR(500) UNIQUE,
     creator_id BIGINT,
     title VARCHAR(500),
     file_id BIGINT, -- from tg
     max_questions_number INT,
-    test_type VARCHAR(255),
+    test_type VARCHAR(255), -- enum
     PRIMARY KEY (unique_title),
-    FOREIGN KEY (creator_id) REFERENCES users(chat_id)
+    FOREIGN KEY (id) REFERENCES users(id)
 --     FOREIGN KEY (test_type) REFERENCES test_types(type)
 );
 
@@ -32,40 +28,46 @@ CREATE TABLE IF NOT EXISTS tests (
 -- );
 
 CREATE TABLE IF NOT EXISTS variable_answers (
+    id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE,
     text VARCHAR(30) UNIQUE,
-    PRIMARY KEY (text)
+    PRIMARY KEY (id)
 );
+
+CREATE INDEX IF NOT EXISTS variable_answer_text
+    ON variable_answers(text);
 
 CREATE TABLE IF NOT EXISTS variable_questions (
     id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE,
-    test_unique_title VARCHAR(500),
+    test_id BIGINT,
     text VARCHAR(5000),
     image_id BIGINT, -- from tg
     audio_id BIGINT, -- from tg
-    file_id BIGINT, -- from tg
+    file_id BIGINT,  -- from tg
     max_answer_number INT,
-    correct_answer VARCHAR(30),
+    correct_answer_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (correct_answer) REFERENCES variable_answers(text)
+    FOREIGN KEY (test_id) REFERENCES tests(id),
+    FOREIGN KEY (correct_answer_id) REFERENCES variable_answers(id)
 );
 
 CREATE TABLE IF NOT EXISTS variable_questions_answers (
     question_id BIGINT,
-    answer_text VARCHAR(30),
-    PRIMARY KEY (question_id, answer_text),
+    answer_id BIGINT,
+    PRIMARY KEY (question_id, answer_id),
     FOREIGN KEY (question_id) REFERENCES variable_questions(id),
-    FOREIGN KEY (answer_text) REFERENCES variable_answers(text)
+    FOREIGN KEY (answer_id) REFERENCES variable_answers(id)
 );
 
 CREATE TABLE IF NOT EXISTS open_questions (
     id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE,
-    test_unique_title VARCHAR(500),
+    test_id BIGINT,
     text VARCHAR(5000),
     image_id BIGINT, -- from tg
     audio_id BIGINT, -- from tg
-    file_id BIGINT, -- from tg
+    file_id BIGINT,  -- from tg
     correct_answer VARCHAR(5000),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (test_id) REFERENCES tests(id)
 );
 
 CREATE TABLE IF NOT EXISTS open_answers (
@@ -74,12 +76,26 @@ CREATE TABLE IF NOT EXISTS open_answers (
     text VARCHAR(5000),
     image_id BIGINT, -- from tg
     audio_id BIGINT, -- from tg
-    file_id BIGINT, -- from tg
+    file_id BIGINT,  -- from tg
     PRIMARY KEY (question_id, user_id),
     FOREIGN KEY (question_id) REFERENCES open_questions(id),
-    FOREIGN KEY (user_id) REFERENCES users(chat_id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- todo нормализовать таблицы (сделать уникальные id везде); у юзера chat_id -> id
+CREATE TABLE IF NOT EXISTS users_tests ( -- сохраненные тесты,
+    user_id BIGINT,                      -- созданные другими
+    test_id BIGINT,                      -- пользователями
+    PRIMARY KEY (user_id, test_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (test_id) REFERENCES tests(id)
+);
 
+
+-- DROP table users_tests;
+-- DROP table open_answers;
+-- DROP table open_questions;
+-- DROP table variable_questions_answers;
+-- DROP table variable_questions;
+-- DROP table variable_answers;
+-- DROP table tests;
 -- DROP table users;
