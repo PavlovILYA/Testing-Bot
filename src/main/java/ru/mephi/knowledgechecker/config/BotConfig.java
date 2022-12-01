5a0952d6fa56d9713eb11b17402e33a2475a1b82
Илюша <79216023478@yandex.ru>
2022-12-01 22:26:22 +0300

    refactor: мелкие правки по стилю

--- src/main/java/ru/mephi/knowledgechecker/config/BotConfig.java
+++ src/main/java/ru/mephi/knowledgechecker/config/BotConfig.java
@@ -3,7 +3,6 @@ package ru.mephi.knowledgechecker.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -27,9 +26,9 @@ public class BotConfig {
                 .url(webhookUrl)
                 .build();
         ResponseEntity<Object> responseDeleteWebhook = restTemplate
-                .postForEntity(telegramApi+"/deleteWebhook", null, Object.class);
+                .postForEntity(telegramApi + "/deleteWebhook", null, Object.class);
         ResponseEntity<Object> responseSetWebhook = restTemplate
-                .postForEntity(telegramApi+"/setWebhook", webHookCreateDto, Object.class);
+                .postForEntity(telegramApi + "/setWebhook", webHookCreateDto, Object.class);
 
         log.info("Delete webhook: {}", responseDeleteWebhook.getBody());
         log.info("Set webhook again: {}", responseSetWebhook.getBody());
