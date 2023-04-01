dac699538140e00c45545e40e52c3974acce9b72
Илюша <79216023478@yandex.ru>
2023-04-01 16:45:24 +0300

    refactor: разбить проект на модули

---
+++ gift-parser/src/main/java/ru/mephi/giftparser/GiftParserApplication.java
@@ -0,0 +1,13 @@
+package ru.mephi.giftparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftParserApplication.class, args);
+	}
+
+}
