95f82409416e1c37ec32e8d6d7abdc65dd437819
Илюша <79216023478@yandex.ru>
2023-04-09 01:26:20 +0300

    feat: на коленке парсинг с использованием регулярных выражений

--- gift-parser/src/main/java/ru/mephi/knowledgechecker/converter/impl/GiftConverter.java
+++ gift-parser/src/main/java/ru/mephi/knowledgechecker/converter/impl/GiftConverter.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Component
 public class GiftConverter extends AbstractFileConverter {
     public static final String TITLE = "::";
-    public static final String TITLE_PART = ":";
     public static final String BEGIN_ANSWERS = "{";
     public static final String BEGIN_ANSWERS_RE = "\\{";
     public static final String END_ANSWERS = "}";
@@ -28,6 +27,7 @@ public class GiftConverter extends AbstractFileConverter {
     public static final String F = "F";
     public static final String FALSE_RU = "Неверно";
     public static final String LIKELIHOOD = "%";
+    public static final String PERCENT_REGEXP = LIKELIHOOD + "\\d+\\.?\\d*" + LIKELIHOOD + ".*";
     public static final String NEXT_LINE = "\n";
     public static final String Q = "Q";
     public static final List<String> BINARY_ANSWERS = List.of(TRUE, T, FALSE, F);
