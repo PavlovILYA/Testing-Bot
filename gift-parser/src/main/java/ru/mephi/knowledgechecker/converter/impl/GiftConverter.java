f9f56857a7fe99e017953679171740f9d0120b88
Илюша <79216023478@yandex.ru>
2023-04-08 22:34:51 +0300

    fix: начать реализовывать парсинг с использованием регулярных выражений

--- gift-parser/src/main/java/ru/mephi/knowledgechecker/converter/impl/GiftConverter.java
+++ gift-parser/src/main/java/ru/mephi/knowledgechecker/converter/impl/GiftConverter.java
@@ -1,34 +1,36 @@
 package ru.mephi.knowledgechecker.converter.impl;
 
 import org.springframework.stereotype.Component;
-import ru.mephi.knowledgechecker.exception.FileParsingException;
 import ru.mephi.knowledgechecker.model.answer.VariableAnswer;
 import ru.mephi.knowledgechecker.model.question.OpenQuestion;
 import ru.mephi.knowledgechecker.model.question.VariableQuestion;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 
 @Component
 public class GiftConverter extends AbstractFileConverter {
-    private static final String TITLE = "::";
-    private static final char TITLE_PART = ':';
-    private static final char BEGIN_ANSWERS = '{';
-    private static final char END_ANSWERS = '}';
-    private static final char INCORRECT_ANSWER = '~';
-    private static final char CORRECT_ANSWER = '=';
-    private static final String COMMENT = "//";
-    private static final char COMMENT_PART = '/';
-    private static final char ANSWER_COMMENT = '#';
-    private static final String TRUE = "TRUE";
-    private static final char T = 'T';
-    private static final String FALSE = "FALSE";
-    private static final char F = 'F';
-    private static final char LIKELIHOOD = '%';
-    private static final String NEXT_LINE = "\n";
-    private static final String Q = "Q";
+    public static final String TITLE = "::";
+    public static final String TITLE_PART = ":";
+    public static final String BEGIN_ANSWERS = "{";
+    public static final String BEGIN_ANSWERS_RE = "\\{";
+    public static final String END_ANSWERS = "}";
+    public static final String END_ANSWERS_RE = "\\}";
+    public static final String INCORRECT_ANSWER = "~";
+    public static final String CORRECT_ANSWER = "=";
+    public static final String COMMENT = "//";
+    public static final String COMMENT_PART = "/";
+    public static final String ANSWER_COMMENT = "#";
+    public static final String TRUE = "TRUE";
+    public static final String T = "T";
+    public static final String TRUE_RU = "Верно";
+    public static final String FALSE = "FALSE";
+    public static final String F = "F";
+    public static final String FALSE_RU = "Неверно";
+    public static final String LIKELIHOOD = "%";
+    public static final String NEXT_LINE = "\n";
+    public static final String Q = "Q";
+    public static final List<String> BINARY_ANSWERS = List.of(TRUE, T, FALSE, F);
 
     @Override
     protected String getSuffix() {
@@ -67,50 +69,63 @@ public class GiftConverter extends AbstractFileConverter {
 
     @Override
     protected Long parseFileContent(Long userId, File file) { // todo название теста получать http-параметром
-        try (FileReader reader = new FileReader(file)) {
-            int c;
-            int p = -1;
-            while ((c = reader.read()) != -1) {
-                p = parseSymbol(p, c, reader);
-            }
-        } catch (IOException e) {
-            throw new FileParsingException(e.getMessage());
-        }
+//        try (FileReader reader = new FileReader(file)) {
+//            int c;
+//            int p = -1;
+//            while ((c = reader.read()) != -1) {
+//                p = parseSymbol(p, c, reader);
+//            }
+//        } catch (IOException e) {
+//            throw new FileParsingException(e.getMessage());
+//        }
         return null;
     }
 
-    private char parseSymbol(int p, int c, FileReader reader) throws IOException {
-        switch ((char) c) {
-            case TITLE_PART:
-                readTitle(p, c, reader);
-                break;
-            case BEGIN_ANSWERS:
-                break;
-            case END_ANSWERS:
-                break;
-            case INCORRECT_ANSWER:
-                break;
-            case CORRECT_ANSWER:
-                break;
-            case COMMENT_PART:
-                break;
-            case ANSWER_COMMENT:
-                break;
-            case T:
-                break;
-            case F:
-                break;
-            case LIKELIHOOD:
-                break;
-            default:
-                throw new FileParsingException("Unknown symbol: " + c);
-        }
-        return 0;
-    }
-
-    private void readTitle(int p, int c, FileReader reader) throws IOException {
-//        if ((char) p != TITLE_PART) {
-//            reader.read()
+//    protected Long parseFileContentOld(Long userId, File file) { // todo название теста получать http-параметром
+//        try (FileReader reader = new FileReader(file)) {
+//            int c;
+//            int p = -1;
+//            while ((c = reader.read()) != -1) {
+//                p = parseSymbol(p, c, reader);
+//            }
+//        } catch (IOException e) {
+//            throw new FileParsingException(e.getMessage());
 //        }
-    }
+//        return null;
+//    }
+//
+//    private char parseSymbol(int p, int c, FileReader reader) throws IOException {
+//        switch ((char) c) {
+//            case TITLE_PART:
+//                readTitle(p, c, reader);
+//                break;
+//            case BEGIN_ANSWERS:
+//                break;
+//            case END_ANSWERS:
+//                break;
+//            case INCORRECT_ANSWER:
+//                break;
+//            case CORRECT_ANSWER:
+//                break;
+//            case COMMENT_PART:
+//                break;
+//            case ANSWER_COMMENT:
+//                break;
+//            case T:
+//                break;
+//            case F:
+//                break;
+//            case LIKELIHOOD:
+//                break;
+//            default:
+//                throw new FileParsingException("Unknown symbol: " + c);
+//        }
+//        return 0;
+//    }
+//
+//    private void readTitle(int p, int c, FileReader reader) throws IOException {
+////        if ((char) p != TITLE_PART) {
+////            reader.read()
+////        }
+//    }
 }
